5cb8ebc51325ae1a646aaae4f579aca9fa72a3fc
jyun <jyunakaike@gmail.com>
2022-02-10 18:37:48 -0400

    feat: ListOfCategories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
 
 export const App = () => (
-  <Category />
+  <ListOfCategories />
 )
 
 
