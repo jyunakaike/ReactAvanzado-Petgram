14f7d724798c8e5e7429e27cfa9164ceda420d3b
jyun <jyunakaike@gmail.com>
2022-02-11 12:51:16 -0400

    feat: ListOfCategory category when scroll with UseEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -5,21 +5,42 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch('https://react-avanzado-petgram-jyunakaike.vercel.app/categories')
-    .then(res => res.json())
-    .then(response=> {
-      setCategories(response)
-    })
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
   }, [])
 
+  useEffect(() => {
+    const onScroll = (e)=> {
+      const newShowFixed = window.scrollY > 200 
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
 
-  return (
-    <List>
+    document.addEventListener('scroll',onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  },[showFixed])
+
+  const renderList = (fixed) => (
+    <List className={(fixed) ? 'fixed' : ''} >
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+
+  return (
+    <React.Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </React.Fragment>
+
+
   )
 }
