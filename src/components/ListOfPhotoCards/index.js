6588df2781426cce2101d149c173e2964e4fb8c3
jyun <jyunakaike@gmail.com>
2022-02-13 16:17:40 -0400

    feat: fetching data with graphql and react apollo

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+// components
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = () => {
+const withPhotos = gql`
+  query getPhotos {
+    photos{
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
 
-  const array = [1,2,3,4,5,8,9]
+export const ListOfPhotoCards = () => {
+  const {loading, error, data } = useQuery(withPhotos);
+
+  if(error) {
+    return <h2> Internal Server Error</h2>;
+  }
+  if(loading){
+    return <h2> loading...</h2>
+  }
+
   return (
     <React.Fragment>
       {
-        array.map(card => <PhotoCard key={card} id={card}></PhotoCard>)
+        data.photos.map(card => <PhotoCard key={card.id} {...card}>  </PhotoCard>)
       }
     </React.Fragment>
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {App} from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { App } from './App'
+
+const client = new ApolloClient({
+  uri:"https://react-avanzado-petgram-jyunakaike.vercel.app/graphql",
+  cache: new InMemoryCache(),
+})
 
 
-ReactDOM.render( <App /> , document.getElementById('app'))
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'));
