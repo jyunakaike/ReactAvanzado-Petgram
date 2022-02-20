781627d82208a4391bd1f3766797d005fa9e29a6
jyun <jyunakaike@gmail.com>
2022-02-20 13:03:23 -0400

    Mutacion para iniciar sesion

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { RegisterMutation } from '../container/RegisterMutation';
+import { LoginMutation } from '../container/LoginMutation';
 
 import { UserForm } from '../components/UserForm';
 
@@ -11,20 +12,34 @@ export const NotRegisteredUser = () => {
     <React.Fragment>
       <RegisterMutation>
         {
-          (register, {data, loading, error}) => {
-            const onSubmit = ({email, password})=> {
-              const input= {email, password}
-              const variables = {input}
-              register({variables}).then(activateAuth)
+          (register, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              // register({ variables }).then(activateAuth)
+              register({variables})
             }
-            const errorMsg= error && `El usuario ya existe o hay un problema.`
+            const errorMsg = error && `El usuario ya existe o hay un problema.`
 
             return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Registrarse' />
           }
         }
       </RegisterMutation>
 
-      <UserForm aproveAuth={activateAuth} title='Iniciar Sesion' />
+      <LoginMutation>
+        {
+          (login, { data, loading ,error})=> {
+            const onSubmit = ({email, password})=> {
+              const input = {email, password}
+              const variables = {input} 
+              login( {variables}).then(activateAuth)
+            }
+            const errorMsg = error && `e-mail o password incorrecto`
+
+            return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Iniciar Sesion' />
+          }
+        }
+      </LoginMutation>
     </React.Fragment>
   )
 }
